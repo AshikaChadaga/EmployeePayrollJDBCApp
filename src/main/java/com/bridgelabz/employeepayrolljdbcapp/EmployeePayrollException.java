93e3ac36fed7a057eaa52d05d1c5d38e40deb970
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-27 16:55:15 +0530

    ADDED: Custom Exceptions

---
+++ src/main/java/com/bridgelabz/employeepayrolljdbcapp/EmployeePayrollException.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.employeepayrolljdbcapp;
+
+public class EmployeePayrollException extends RuntimeException {
+
+    enum ExceptionType {
+        CONNECTION_FAIL, CANNOT_EXECUTE_QUERY, UPDATE_FAILED
+    }
+
+    ExceptionType exceptionType;
+
+    public EmployeePayrollException(ExceptionType exceptionType, String message) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+}
