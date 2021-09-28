00f249e3e751277a172abdba6e5d7ac753d6d8ca
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-28 14:46:14 +0530

    REFACTORED: Logic to add new EMployee details to Payroll

--- src/main/java/com/bridgelabz/employeepayrolljdbcapp/EmployeePayrollDBService.java
+++ src/main/java/com/bridgelabz/employeepayrolljdbcapp/EmployeePayrollDBService.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.employeepayrolljdbcapp;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -286,6 +287,31 @@ public class EmployeePayrollDBService {
 
 		}
 		return employeePayrollList;
+	}
+	
+	public EmployeePayrollData addEmployeeToPayroll(String name, double salary, LocalDate startDate, String gender) {
+		
+		int employeeId = -1;
+		EmployeePayrollData employeePayrollData = null;
+		String sql = String.format("INSERT INTO employee_payroll (name, gender, salary, start) VALUES ('%s', '%s', '%s', '%s')", name, gender, salary, Date.valueOf(startDate));
+		
+		try (Connection connection = this.getConnection()){
+			
+			Statement statement = connection.createStatement();
+			int rowAffected = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);
+			if(rowAffected == 1) {
+				ResultSet resultSet = statement.getGeneratedKeys();
+				if(resultSet.next())
+					employeeId = resultSet.getInt(1);
+			}
+			employeePayrollData = new EmployeePayrollData(employeeId, name, salary, startDate);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return employeePayrollData;
+		
 	}
 	
 }
